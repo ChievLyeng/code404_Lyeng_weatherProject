9e08e48539dc1f36e3238671e6ff72f365e5e94b
004 <anbschool0004@004ui-Macmini.local>
2023-09-13 19:37:36 +0700

    fetchapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 import './App.css'
+import { WeatherShow } from './components/WeatherShow';
 
 function App() {
+  const [currentLocation,setCurrentLocation] = useState('');
+  const [weather,setWeather] = useState(null);
+
+  const getLocation = async () => {
+    try {
+      // const url = `https://api.ipdata.co?api-key=831d679737f839691532d3ebcc79aa340fc410f92b1cc56ca1d7573a`;
+      const currLocation = await axios.get(url);
+      setCurrentLocation(currLocation.data.city);
+
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Phnom Penh&appid=2f43c83c40b4e2ddc001d7b04ec46eab&units=metric`)
+      setWeather(response.data);
+    
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
 
   return (
     <>
+
      <div>
-      Weather App
+        <WeatherShow weather={weather} />
      </div>
         
     </>
