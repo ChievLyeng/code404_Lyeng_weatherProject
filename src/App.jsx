2222f013875a0343aa5ee0054d5dc752778daacc
004 <anbschool0004@004ui-Macmini.local>
2023-09-14 20:40:31 +0700

    forecastweather

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,67 @@ import './App.css'
 import { WeatherShow } from './components/WeatherShow';
 
 function App() {
-  const [currentLocation,setCurrentLocation] = useState('');
-  const [weather,setWeather] = useState(null);
+  
+  const [currentLocation,setCurrentLocation] = useState('')
+  const [weather,setWeather] = useState(null)
+  const [ForecastWeather,setForecastWeather] = useState(null)
+  const [loading,setLoading] = useState(true);
+  
+  //Current weather
+    const getLocation = async () => {
+      try {
 
-  const getLocation = async () => {
-    try {
-      const url = `https://api.ipdata.co?api-key=831d679737f839691532d3ebcc79aa340fc410f92b1cc56ca1d7573a`;
-      const currLocation = await axios.get(url);
-      setCurrentLocation(currLocation.data.city);
-
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${currLocation.data.city}&appid=2f43c83c40b4e2ddc001d7b04ec46eab&units=metric`)
-      setWeather(response.data);
-    
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    getLocation();
-  }, []);
-
-
-  return (
-    <>
-
-     <div>
-        <WeatherShow weather={weather} />
-     </div>
+        //currentLocation Api
+        const url = `https://api.ipdata.co?api-key=831d679737f839691532d3ebcc79aa340fc410f92b1cc56ca1d7573a`;
+        const currLocation = await axios.get(url);
+        setCurrentLocation(currLocation.data.city);
+        console.log("city",currLocation.data.city)
         
-    </>
-  )
+        //currentweather Api
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${currLocation.data.city}&appid=2f43c83c40b4e2ddc001d7b04ec46eab&units=metric`
+        );
+        setWeather(response.data);
+        console.log(response.data)
+
+        //forecasweather Api
+        const result = await axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${currLocation.data.city}&appid=2f43c83c40b4e2ddc001d7b04ec46eab&units=metric`
+          );
+          setForecastWeather(result.data);
+          console.log("Forecast : ",result.data)
+
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+        
+      }
+    };
+  
+    useEffect(() => {
+      getLocation();
+    }, []);
+
+
+
+return (
+  <>
+    <div>
+        {!loading ? (
+     <div className='container'>
+     <p className='loading'>App is Loading...</p>
+     <img className='loading-icon' src={loadingicon} alt="loading-icon" />
+     </div>
+    ) : (
+      weather && (
+        <div className='container' >
+          <WeatherFetching weather={weather} ForecastWeather={ForecastWeather} />
+        </div>
+      )
+    )}
+    </div>
+  </>
+)
 }
 
 export default App
--- src/components/WeatherShow.jsx
+++ src/components/WeatherShow.jsx
@@ -1,13 +1,13 @@
 import { useEffect,useState } from 'react';
-import clearsky from '../components/img/clearsky.svg';
-import fewclouds from '../components/img/fewclouds.svg';
-import mist from '../components/img/mist.svg';
-import rain from '../components/img/rain.svg';
-import snow from '../components/img/snow.svg';
-import thunderstrom from '../components/img/thunderstorm.svg';
-// import brokenclouds from '../components/img/brokenclouds.svg';
-// import scatteredclouds from '../components/img/scatteredclouds.svg';
-// import showerain from '../components/img/showerain.svg';
+import clearsky from '../img/clearsky.svg';
+// import brokenclouds from '../brokenclouds.svg';
+import fewclouds from '../img/fewclouds.svg';
+import mist from '../img/mist.svg';
+import rain from '../img/rain.svg';
+// import scatteredclouds from '../scatteredclouds.svg';
+// import showerain from '../showerain.svg';
+import snow from '../img/snow.svg';
+import thunderstrom from '../img/thunderstorm.svg';
 
 
 const iconMap = {
@@ -20,33 +20,30 @@ const iconMap = {
     
 };
 
-
-export const WeatherShow = ({weather}) => {
-
-    //getCurrent day
-    const getDate = () => {
-        const today = new Date();
-        const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-        const month = today.getMonth() + 1;
-        const year = today.getFullYear();
-        const date = weekday[today.getDay()];
-        
-        return ` ${date}, ${month}, ${year}`;
-      }
-
-      const [date,setDate] = useState(getDate());
-
-      //Check if weather is aviailable before accessing its properties
-      const cityName = weather ? weather.name : "N/A";
-      const temperature = weather ? Math.round(weather.main.temp) : "N/A";
-      const humidity = weather ? weather.main.humidity : "N/A";
-      const condition = weather ? weather.weather[0].main : "N/A";
-      const wind = weather ? weather.wind.speed : "N/A"
-      const icon = iconMap[condition]
-
+export const WeatherShow = ({ForecastWeather,weather,day2,day3,day4,day5,date
+                        }) => {
+    
+    const cityName = weather ? weather.name : 'N/A';
+    const temperature = weather ? Math.round(weather.main.temp) : "N/A";
+    const humidity = weather ? weather.main.humidity : "N/A";
+    const condition = weather ? weather.weather[0].main : "N/A";
+    const wind = weather ? weather.wind.speed : "N/A"
+    const weatherday2 = ForecastWeather ? Math.round(ForecastWeather.list[8].main.temp) : "N/A"
+    const weatherday3 = ForecastWeather ? Math.round(ForecastWeather.list[16].main.temp) : "N/A"
+    const weatherday4 = ForecastWeather ? Math.round(ForecastWeather.list[24].main.temp) : "N/A"
+    const weatherday5 = ForecastWeather ? Math.round(ForecastWeather.list[32].main.temp) : "N/A"
+    const conditionday2 = ForecastWeather? ForecastWeather.list[7].weather[0].main : "N/A"
+    const conditionday3 = ForecastWeather? ForecastWeather.list[15].weather[0].main : "N/A"
+    const conditionday4 = ForecastWeather? ForecastWeather.list[23].weather[0].main : "N/A"
+    const conditionday5 = ForecastWeather? ForecastWeather.list[31].weather[0].main : "N/A"
+    const icon = iconMap[condition]
+    
+      
       return (
+        <>
         <div>
             <div className="container">
+            
                 {/* weather info */}
                 <div className='weather-info-container'>
                 <p className='weather-date'> {date} </p>
@@ -86,12 +83,41 @@ export const WeatherShow = ({weather}) => {
                             <div className='weather-condition'><p> {humidity} &#x25;</p></div>
                             </div>
                         </div>
-            
                     </div>
                 </div>
                 </div>
                 </div>  
+
+                <div className='weather-week-container'>
+            <div className='nextday-weather'>
+                    <p className='nextday-title'> {day2} </p>
+                    <img className='nextday-weather-icon' src={iconMap[conditionday2]} />  
+                    <p className='nextday-title'>  {weatherday2} &deg;C  </p>
+
+            </div>
+
+            <div className='nextday-weather'>
+                    <p className='nextday-title'> {day3} </p>
+                    <img className='nextday-weather-icon' src={iconMap[conditionday3]} />
+                    <p className='nextday-title'>{weatherday3} &deg;C</p>
+            </div>
+
+            <div className='nextday-weather'>
+                <p className='nextday-title'> {day4} </p>
+                <img className='nextday-weather-icon' src={iconMap[conditionday4]} />
+                <p className='nextday-title'>{weatherday4} &deg;C</p>
+            </div>
+
+            <div className='nextday-weather'>
+                <p className='nextday-title'> {day5} </p>
+                <img className='nextday-weather-icon' src={iconMap[conditionday5]} />
+                <p className='nextday-title'>{weatherday5} &deg;C</p>
+            </div>
+
+            </div>
             </div>
         </div>
-      );
+        </>
+      )
+
 }
