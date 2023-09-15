2a981443fec34ab070ca883bc0edb0a7937c7037
004 <anbschool0004@004ui-Macmini.local>
2023-09-15 16:43:29 +0700

    forecastweather

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [currentLocation,setCurrentLocation] = useState('')
   const [weather,setWeather] = useState(null)
   const [ForecastWeather,setForecastWeather] = useState(null)
-  const [loading,setLoading] = useState(true);
+  const [loading,setLoading] = useState(false);
   
   //Current weather
     const getLocation = async () => {
@@ -17,8 +17,8 @@ function App() {
         //currentLocation Api
         const url = `https://api.ipdata.co?api-key=831d679737f839691532d3ebcc79aa340fc410f92b1cc56ca1d7573a`;
         const currLocation = await axios.get(url);
-        setCurrentLocation(currLocation.data.city);
-        console.log("city",currLocation.data.city)
+        setCurrentLocation(currLocation.data.city); 
+        console.log(currLocation.data.city)
         
         //currentweather Api
         const response = await axios.get(
@@ -29,15 +29,36 @@ function App() {
 
         //forecasweather Api
         const result = await axios.get(
-          `https://api.openweathermap.org/data/2.5/forecast?q=${currLocation.data.city}&appid=2f43c83c40b4e2ddc001d7b04ec46eab&units=metric`
+          `https://api.openweathermap.org/data/2.5/forecast?q=${currLocation?.data.city}&appid=2f43c83c40b4e2ddc001d7b04ec46eab&units=metric`
           );
           setForecastWeather(result.data);
           console.log("Forecast : ",result.data)
 
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching data from api", error);
         setLoading(false);
-        
+
+          //this is prevention when currentlocation api is not work it will fetch the weather in phnom penh city
+          try {
+            //currentweather Api
+            const response = await axios.get(
+              `https://api.openweathermap.org/data/2.5/weather?q=Phnom Penh&appid=2f43c83c40b4e2ddc001d7b04ec46eab&units=metric`
+            );
+            setWeather(response.data);
+            console.log("weather2",response.data)
+
+            //forecasweather Api
+            const result = await axios.get(
+              `https://api.openweathermap.org/data/2.5/forecast?q=Phnom Penh&appid=2f43c83c40b4e2ddc001d7b04ec46eab&units=metric`
+              );
+              setForecastWeather(result.data);
+              console.log("Forecast2 : ",result.data)
+              console.log("2",loading)
+
+          } catch (error) {
+            console.error("Error fetching data from api", error);
+            setLoading(true);
+          }
       }
     };
   
@@ -45,23 +66,23 @@ function App() {
       getLocation();
     }, []);
 
+   
 
 
 return (
   <>
     <div>
-        {!loading ? (
-     <div >
-     <p className='loading'>App is Loading...</p>
-     <img className='loading-icon' src={loadingicon} alt="loading-icon" />
-     </div>
+        {loading ? (
+        <div >
+          <p className='loading'>Error Fetching Data From Api...</p>
+          <img className='loading loading-icon' src={loadingicon} alt="loading-icon" />
+    </div>
     ) : (
-      weather && (
-        <div>
+      weather && ForecastWeather && (
           <WeatherFetching weather={weather} ForecastWeather={ForecastWeather} />
-        </div>
       )
     )}
+    
     </div>
   </>
 )
